ee73afc320a612c1e9a30eaba7a8b81222d28ff3
zickz4g <lucianorosende@gmail.com>
2022-06-15 17:20:53 -0300

    fonts added

--- App.js
+++ App.js
@@ -1,26 +1,36 @@
 import React, {useState} from 'react';
-import { View, SafeAreaView } from 'react-native';
+import { useFonts } from 'expo-font';
+import { View, SafeAreaView, ActivityIndicator } from 'react-native';
 import { styles } from './styles';
 import { Header } from "./components/index"
 import StartGame from "./screens/start-game/index"
 import GameScreen from './screens/game-screen';
 
+
 export default function App() {
 
   const [userNumber, setUserNumber] = useState();
+
+  // no me reconoce el font
+
+  // const [loaded] = useFonts({
+  //   CONSOLA: require("./assets/fonts/CONSOLA.TTF"),
+  // });
+
+  // if(!loaded){
+  //  return <ActivityIndicator size="large" color={theme.colors.primary} />
+  // }
 
   const onStartGame = (selectedNumber) => {
     setUserNumber(selectedNumber);
   }
 
   let content = userNumber ? ( 
-    <GameScreen userOptions={userNumber} />
+    <GameScreen userOptions={userNumber}/>
     ) : (
     <StartGame onStartGame={onStartGame} />
     );
     
-
-
   return (
     <SafeAreaView style={styles.container}>
     <View style={styles.container}>
